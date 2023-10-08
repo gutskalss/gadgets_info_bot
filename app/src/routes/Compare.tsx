a2db249a685706a343e9d122ffa41ec813574638
Sergey Gutskal <gutskalss@Sergeys-MacBook-Air.local>
2023-10-08 20:29:26 +0300

    feat: integrate telegram BackButton

--- app/src/routes/Compare.tsx
+++ app/src/routes/Compare.tsx
@@ -1,27 +1,32 @@
-import { Show, useContext } from 'solid-js'
+import { Show, onCleanup, useContext } from 'solid-js'
+import { useNavigate } from '@solidjs/router'
 
 import { RootContext } from '@/App'
-import { GadgetsTable, ToMain } from '@/components/layout'
+import { GadgetsTable } from '@/components/layout'
 import { gadgets } from '@/mock/gadgets'
+import { tg } from '@/constants'
 
 export const Compare = () => {
   const { selectedGadgets } = useContext(RootContext)
+  const navigate = useNavigate()
 
   const selectedGadgetsData = gadgets.filter(({ gadgetId }) =>
     selectedGadgets().includes(gadgetId)
   )
 
+  tg.BackButton.show()
+  tg.BackButton.onClick(() => navigate('/'))
+
+  onCleanup(tg.BackButton.hide)
+
   return (
-    <>
-      <div class='container'>
-        <Show
-          when={selectedGadgetsData.length}
-          fallback={<div class='is-size-3'>No gadgets selected</div>}
-        >
-          <GadgetsTable selectedGadgets={selectedGadgetsData} />
-        </Show>
-      </div>
-      <ToMain />
-    </>
+    <div class='container'>
+      <Show
+        when={selectedGadgetsData.length}
+        fallback={<div class='is-size-3'>No gadgets selected</div>}
+      >
+        <GadgetsTable selectedGadgets={selectedGadgetsData} />
+      </Show>
+    </div>
   )
 }
--- app/src/routes/GadgetDetails.tsx
+++ app/src/routes/GadgetDetails.tsx
@@ -1,25 +1,29 @@
-import { ParentComponent, ComponentProps, Show } from 'solid-js'
-import { useParams } from '@solidjs/router'
+import { ParentComponent, ComponentProps, Show, onCleanup } from 'solid-js'
+import { useNavigate, useParams } from '@solidjs/router'
 
 import { gadgets } from '@/mock/gadgets'
-import { GadgetsTable, ToMain } from '@/components/layout'
+import { GadgetsTable } from '@/components/layout'
+import { tg } from '@/constants'
 
 export const GadgetDetails: ParentComponent<ComponentProps<'div'>> = () => {
   const { id } = useParams()
+  const navigate = useNavigate()
 
   const selectedGadget = gadgets.find(({ gadgetId }) => gadgetId === +id)
 
+  tg.BackButton.show()
+  tg.BackButton.onClick(() => navigate('/'))
+
+  onCleanup(tg.BackButton.hide)
+
   return (
-    <>
-      <div class='container'>
-        <Show
-          when={selectedGadget}
-          fallback={<div class='is-size-3'>Gadget not found</div>}
-        >
-          <GadgetsTable selectedGadgets={[selectedGadget!]} />
-        </Show>
-      </div>
-      <ToMain />
-    </>
+    <div class='container'>
+      <Show
+        when={selectedGadget}
+        fallback={<div class='is-size-3'>Gadget not found</div>}
+      >
+        <GadgetsTable selectedGadgets={[selectedGadget!]} />
+      </Show>
+    </div>
   )
 }
