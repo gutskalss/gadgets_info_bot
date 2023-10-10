c4cdd5cdc55f56c52768c616f2473748d6ea18bc
Sergey Gutskal <gutskalss@Sergeys-MacBook-Air.local>
2023-10-10 08:09:06 +0300

    refactor: gadgets info table layout

--- app/src/components/layout/GadgetsTable/GadgetsTable.tsx
+++ app/src/components/layout/GadgetsTable/GadgetsTable.tsx
@@ -1,8 +1,10 @@
 import { ParentComponent, ComponentProps, For } from 'solid-js'
+import cn from 'classnames'
 
 import { TGadgetInfo } from '@/types/gadgetInfo'
-
 import { config } from './config'
+
+import styles from './GadgetsTable.module.scss'
 
 type TGadgetsTableProps = {
   selectedGadgets: TGadgetInfo[]
@@ -12,42 +14,38 @@ export const GadgetsTable: ParentComponent<
   ComponentProps<'div'> & TGadgetsTableProps
 > = ({ selectedGadgets }) => {
   return (
-    <div class='table-container'>
+    <div class={cn('table-container', styles.root)}>
       <table class='table is-bordered'>
         <thead>
           <tr>
-            <th></th>
             <For each={selectedGadgets}>
               {(gadget) => <th>{gadget.name}</th>}
             </For>
           </tr>
         </thead>
         <tbody>
-          {config.map(({ categoryKey, title, fields }) => (
-            <>
-              <tr>
-                <td colSpan={selectedGadgets.length + 1}>
-                  <b>{title}</b>
-                </td>
-              </tr>
-              {fields.map(({ fieldKey, title }) => (
-                <>
-                  <tr>
-                    <td>
-                      <b>{title}</b>
-                    </td>
+          {config.map(({ categoryKey, title: category, fields }) =>
+            fields.map(({ fieldKey, title: subcategory }) => (
+              <>
+                <tr>
+                  <td class='category-row' colSpan={selectedGadgets.length + 1}>
+                    <b>
+                      <span>{category}</span> / <span>{subcategory}</span>
+                    </b>
+                  </td>
+                </tr>
 
-                    {/* TODO fix typing */}
-                    <For each={selectedGadgets}>
-                      {(gadget) => (
-                        <td>{gadget.specifications[categoryKey][fieldKey]}</td>
-                      )}
-                    </For>
-                  </tr>
-                </>
-              ))}
-            </>
-          ))}
+                <tr>
+                  {/* TODO fix typing */}
+                  <For each={selectedGadgets}>
+                    {(gadget) => (
+                      <td>{gadget.specifications[categoryKey][fieldKey]}</td>
+                    )}
+                  </For>
+                </tr>
+              </>
+            ))
+          )}
         </tbody>
       </table>
     </div>
